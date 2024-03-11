0c00e1032234f5aa52a28f5424efb6786a8e36f3
Mateus <mponeres3@gmail.com>
2024-03-11 15:19:58 -0300

    Adicionando cypress dashboard

---
+++ cypress.config.js
@@ -0,0 +1,21 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  projectId: "pcr8bb",
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reporterDir: 'mochawesome-report',
+    overwrite: false,
+    reportFilename: 'index.html',
+    html: true,
+    json: false,
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'http://lojaebac.ebaconline.art.br/',
+  },
+})
